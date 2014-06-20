04e30cc0264739fa661b24779c860a6929fd4eae
Suvamsh Shivaprasad <sshivaprasad@rgmadvisors.com>
2014-06-20 14:51:08 -0500

    new Pathset

--- src/main/scala/com/rgm/file/PathSet.scala
+++ src/main/scala/com/rgm/file/PathSet.scala
@@ -10,49 +10,60 @@ import com.rgm.file.Path
 /**
  * Created by sshivaprasad on 6/18/14.
  */
+
+
+// class PathSet (root: com.rgm.file.Path, matcher: PathMatcher, maxDepth: Int, options: LinkOption*) extends Traversable[Path] {
 //
-//trait PathSet[Path] extends Traversable[Path] {
-//  def foreach[U](f: Path => U)
+//  override def foreach[U](f: (com.rgm.file.Path => U)): Unit = {
+//    var d: Int = maxDepth
+//    if(root.exists()) {
+//      Files.walkFileTree(root.jpath,
+//        new SimpleFileVisitor[JPath] {
+//
+//          override def preVisitDirectory(dir: JPath, attrs: BasicFileAttributes) : FileVisitResult = {
+//            if(d <= 0) {
+//              return FileVisitResult.SKIP_SUBTREE
+//            }
+//            else {
+//              if(matcher.matches(Path(dir))) {
+//                f(com.rgm.file.Path(dir))
+//              }
+//              d-=1
+//              FileVisitResult.CONTINUE
+//            }
+//          }
+//
+//          override def visitFile(file: JPath,attrs: BasicFileAttributes ) : FileVisitResult = {
+//            if(matcher.matches(Path(file)))
+//              f(com.rgm.file.Path(file))
+//            FileVisitResult.CONTINUE
+//          }
+//
+//          override def postVisitDirectory(dir: JPath, e: IOException) : FileVisitResult = {
+//            d+=1
+//            FileVisitResult.CONTINUE
+//          }
+//        })
+//    }
+//  }
 //}
 
- class PathSet (root: com.rgm.file.Path, matcher: PathMatcher, maxDepth: Int, options: LinkOption*) extends Traversable[Path] {
+class PathSet(root: Path) extends Traversable[Path] {
+  private var memberPath: Path = root
 
-  override def foreach[U](f: (com.rgm.file.Path => U)): Unit = {
-    var d: Int = maxDepth
-    if(root.exists()) {
-      Files.walkFileTree(root.jpath,
-        new SimpleFileVisitor[JPath] {
-
-          override def preVisitDirectory(dir: JPath, attrs: BasicFileAttributes) : FileVisitResult = {
-            if(d <= 0) {
-              return FileVisitResult.SKIP_SUBTREE
-            }
-            else {
-              if(matcher.matches(Path(dir))) {
-                f(com.rgm.file.Path(dir))
-              }
-              d-=1
-              FileVisitResult.CONTINUE
-            }
-          }
-
-          override def visitFile(file: JPath,attrs: BasicFileAttributes ) : FileVisitResult = {
-            if(matcher.matches(Path(file)))
-              f(com.rgm.file.Path(file))
-            FileVisitResult.CONTINUE
-          }
-
-          override def postVisitDirectory(dir: JPath, e: IOException) : FileVisitResult = {
-            d+=1
-            FileVisitResult.CONTINUE
-          }
-        })
-    }
+  override def foreach[U](f: Path => U) {
+    f(memberPath)
   }
 }
+
+final class FilteredPathSet(p: Path, depth: Int, m: PathMatcher) extends PathSet {
+  private var memberPath1: Path
+}
+
+final class CompoundPathSet()
 object PathSet {
 
   def apply(root: Path, matcher: PathMatcher, maxDepth: Int, options: LinkOption*): PathSet =
-    new PathSet(root, matcher, maxDepth, options:_*)
+    new PathSet(root)
 
 }
