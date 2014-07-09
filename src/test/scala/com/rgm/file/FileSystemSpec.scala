0703183112f5fa8d955271054c0015476990df2a
Suvamsh Shivaprasad <sshivaprasad@rgmadvisors.com>
2014-07-09 17:05:42 -0500

    create File/Dir updated

--- src/test/scala/com/rgm/file/FileSystemSpec.scala
+++ src/test/scala/com/rgm/file/FileSystemSpec.scala
@@ -7,7 +7,7 @@ import scala.collection.mutable.ListBuffer
 import scala.util.{Try, Random}
 import java.net.URI
 import java.util
-import java.io.{FileOutputStream, PrintStream}
+import java.io.{IOException, FileOutputStream, PrintStream}
 
 
 class FileIOSpec extends FlatSpec with FileSetupTeardown {
@@ -112,49 +112,49 @@ class FileIOSpec extends FlatSpec with FileSetupTeardown {
 
   //createDirectory
   it should "9. create a directory and check its existence" in {
-    val p = new Path(FileSystems.getDefault.getPath(targetGlobal + "test")).createDirectory()
+    val p = new Path(FileSystems.getDefault.getPath(targetGlobal + "test")).createDirectory(false)
     assert(p.exists())
     flagGlobal = true
   }
 
   //createFile
   it should "10. create a file and check its existence" in {
-    val p = new Path(FileSystems.getDefault.getPath(targetGlobal + "test.tmp")).createFile()
+    val p = new Path(FileSystems.getDefault.getPath(targetGlobal + "test.tmp")).createFile(false)
     assert(p.exists())
     flagGlobal = true
   }
 
   //isSame test
   it should "11. check if the file is the same" in {
-    val p = new Path(FileSystems.getDefault.getPath(targetGlobal + "test.tmp")).createFile()
+    val p = new Path(FileSystems.getDefault.getPath(targetGlobal + "test.tmp")).createFile(false)
     assert(p.isSame(p))
     flagGlobal = true
   }
 
   //size test
   it should "12. ensure temp file size is 0" in {
-    val p = new Path(FileSystems.getDefault.getPath(targetGlobal + "test.tmp")).createFile()
+    val p = new Path(FileSystems.getDefault.getPath(targetGlobal + "test.tmp")).createFile(false)
     assert(p.size().get === 0)
     flagGlobal = true
   }
 
   //isReadable test
   it should "13. create a temp file and check if it is readable-> true" in {
-    val p = new Path(FileSystems.getDefault.getPath(targetGlobal + "test.tmp")).createFile()
+    val p = new Path(FileSystems.getDefault.getPath(targetGlobal + "test.tmp")).createFile(false)
     assert(p.isReadable())
     flagGlobal = true
   }
 
   //isWritable test
   it should "14. create a temp file and check if it is writable-> true" in {
-    val p = new Path(FileSystems.getDefault.getPath(targetGlobal + "test.tmp")).createFile()
+    val p = new Path(FileSystems.getDefault.getPath(targetGlobal + "test.tmp")).createFile(false)
     assert(p.isWritable())
     flagGlobal = true
   }
 
   //isExecutable test
   it should "15. create a temp file and check if it is executable-> false" in {
-    val p = new Path(FileSystems.getDefault.getPath(targetGlobal + "test.tmp")).createFile()
+    val p = new Path(FileSystems.getDefault.getPath(targetGlobal + "test.tmp")).createFile(false)
     assert(!p.isExecutable)
     flagGlobal = true
   }
@@ -162,7 +162,7 @@ class FileIOSpec extends FlatSpec with FileSetupTeardown {
   //isSymbolicLink test
   it should "16. creates a SymLink using NIO and ensures it is a symbolic link" in {
     val p = new Path(FileSystems.getDefault.getPath(targetGlobal + "tmp.link"))
-    val q = new Path(FileSystems.getDefault.getPath(targetGlobal + "test.tmp")).createFile()
+    val q = new Path(FileSystems.getDefault.getPath(targetGlobal + "test.tmp")).createFile(false)
     Files.createSymbolicLink(p.jpath,q.jpath)
     assert(p.isSymLink())
     flagGlobal = true
@@ -186,10 +186,10 @@ class FileIOSpec extends FlatSpec with FileSetupTeardown {
 
   it should "19. not resolve symbolic links in toRealPath iff NOFOLLOW_LINKS option is used " in {
     val p = Path(FileSystems.getDefault.getPath(targetGlobal + "tmp.link"))
-    val q = Path(FileSystems.getDefault.getPath(targetGlobal + "testDir/")).createDirectory()
+    val q = Path(FileSystems.getDefault.getPath(targetGlobal + "testDir/")).createDirectory(false)
     Files.createSymbolicLink(p.jpath, q.jpath)
     val pChild = p.resolve("targetFile")
-    val qChild = q.resolve("targetFile").createFile()
+    val qChild = q.resolve("targetFile").createFile(false)
     val shouldFail = Try(pChild.toRealPath(LinkOption.NOFOLLOW_LINKS))
     val shouldSucceed = Try(pChild.toRealPath())
     assert(shouldFail.get.toString == pChild.path)
@@ -264,6 +264,50 @@ class FileIOSpec extends FlatSpec with FileSetupTeardown {
   it should "23. ensure size called on nonExistent file returns None and NOT Some(0)" in {
     val p = Path("/foo/bar/tmp.txt")
     assert(p.size() == None)
+    flagGlobal = true
   }
 
+  it should "24. ensure parent directories are created when createParents=true in createDirectory" in {
+    val tmp1 = Path(targetGlobal + "tmp_dir/tmp_dir2")
+    val tmp2 = Path(targetGlobal + "tmp_dir")
+    val p = Path(targetGlobal + "tmp_dir/tmp_dir2/tmp_dir3")
+    p.createDirectory(true)
+    assert(tmp1.exists() && tmp2.exists())
+    flagGlobal = true
+  }
+
+  it should "25. fail if it exists in createDirectory" in {
+    val p = Path(targetGlobal + "tmp_dir")
+    p.createDirectory(false)
+    val tmp = p
+    try {
+      tmp.createDirectory(false,true)
+    } catch {
+      case e: IOException => assert(true)
+      case _: Throwable => assert(false)
+    }
+    flagGlobal = true
+  }
+
+  it should "24. ensure parent directories are created when createParents=true in createFile" in {
+    val tmp1 = Path(targetGlobal + "tmp_dir/tmp_dir2")
+    val tmp2 = Path(targetGlobal + "tmp_dir")
+    val p = Path(targetGlobal + "tmp_dir/tmp_dir2/tmp_dir3.txt")
+    p.createFile(true)
+    assert(tmp1.exists() && tmp2.exists())
+    flagGlobal = true
+  }
+
+  it should "25. fail if it exists in createFile" in {
+    val p = Path(targetGlobal + "tmp_dir.txt")
+    p.createFile(false)
+    val tmp = p
+    try {
+      tmp.createFile(false,true)
+    } catch {
+      case e: IOException => assert(true)
+      case _: Throwable => assert(false)
+    }
+    flagGlobal = true
+  }
 }
